cec0fd3ed60d9c3932099ce14cbfa8903c5d0786
Sean Walker <seanleewalkerjr@gmail.com>
2021-08-01 12:55:32 -0400

    Initial Commit

---
+++ Swift UI Intro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift UI Intro
+//
+//  Created by User 1 on 8/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift UI Intro/Swift_UI_IntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_IntroApp.swift
+//  Swift UI Intro
+//
+//  Created by User 1 on 8/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_IntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
